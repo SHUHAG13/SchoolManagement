c90c80ed61e3096bb56ea6fa091d5432f3254605
shuhag <shuhag200113@gmail.com>
2025-08-15 15:26:07 +0600

    Add project files.

---
+++ SchoolManagement.Domain/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string? Email { get; set; }
+
+        //Foreign key to Course
+        public int CourseId { get; set; }
+        //Navigation property to Course
+        public Course Course { get; set; } = default!;
+    }
+}
